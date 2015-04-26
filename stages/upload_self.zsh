ea46974bb571d123a2d206e03abe1c937c0e7fc3
Flavius Aspra <flavius.as@gmail.com>
2015-04-26 21:18:37 +0200

    small fixes

--- stages/upload_self.zsh
+++ stages/upload_self.zsh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 
-ssh 'rm -rf /tmp/bootstrap; mkdir /tmp/bootstrap/'
+ssh 'rm -rf /tmp/bootstrap' root@${BOXROOT_IP}
 #TODO long term: when it's done, use git archive - only versioned files get in
 rm -rf /tmp/bootstrap.tar
 tar --exclude-vcs --exclude-vcs-ignore --create --file /tmp/bootstrap.tar .
 scp -q -B /tmp/bootstrap.tar root@${BOXROOT_IP}:/tmp
-ssh 'cd /tmp/bootstrap; tar -xf ../bootstrap.tar'
+ssh 'mkdir /tmp/bootstrap/; cd /tmp/bootstrap/; tar -xf ../bootstrap.tar' root@${BOXROOT_IP}
