6b2e549b80bcb1f41c02c35e04159f7fa8d3ac04
Flavius Aspra <flavius.as@gmail.com>
2015-05-01 19:49:45 +0200

    send more install commands at once

--- stages/bootstrap_admin_infrastructure.zsh
+++ stages/bootstrap_admin_infrastructure.zsh
@@ -3,6 +3,11 @@
 local -a packages_to_install
 packages_to_install=(rxvt-unicode tree htop lsof iotop cpupower hdparm lshw wget curl net-tools ethtool)
 
+local -a ssh_command
 for package in "${packages_to_install[@]}"; do
-    installpkg "${package}"
+    ssh_command+=( "installpkg ${package}" )
 done
+
+set -x
+
+ssh root@${BOXROOT_FQDN} "/bin/zsh -ic '${(j:; :)ssh_command}'"
