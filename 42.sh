9250a5d3e24568b5d090c56b86e5b16615354c6a
Flavius Aspra <flavius.as@gmail.com>
2015-05-01 19:48:35 +0200

    wait for reboot

--- 42.sh
+++ 42.sh
@@ -2,22 +2,36 @@
 
 source variables.sh
 
+function wait_reboot() {
+    # TODO: functions to manage -e/-x state
+    set +e
+    echo 'D means waiting for host to go down, U means waiting to go up'
+    # wait for box to go down first
+    while ping -c 1 ${1} &>/dev/null; do
+        sleep 0.5
+        echo -n 'D'
+    done
+    echo ''
+    # wait until it gets back up again
+    while ! ping -c 1 ${1} &>/dev/null; do
+        echo -n 'U'
+    done
+    set -x
+    set -e
+}
+
 set -e
 set -x
 
-nc -zw 60 ${BOXROOT_FQDN} 22
-./sysinstall
-# wait for box to go down first
-set +x
-while ping -c 1 ${BOXROOT_FQDN} &>/dev/null; do
-    echo -n 'W'
-done
-# wait until it gets back up again
-while ! ping -c 1 ${BOXROOT_FQDN} &>/dev/null; do
-    echo -n 'U'
-done
-set -x
+wait_reboot ${BOXROOT_FQDN}
 # starting sshd also takes a while, wait for it
-nc -zw 60 ${BOXROOT_FQDN} 22
+set +e
+nc -zw 90 ${BOXROOT_FQDN} 22
+set -e
+./sysinstall
+wait_reboot ${BOXROOT_FQDN}
+set +e
+nc -zw 90 ${BOXROOT_FQDN} 22
+set -e
 ./bootstrap
 ./install
