6a895f4c9bccb92fe2a7ee7bf3406ade2d7cfefc
Flavius Aspra <flavius.as@gmail.com>
2015-05-01 15:23:27 +0200

    script which executes all the steps

--- 42.sh
+++ 42.sh
@@ -7,6 +7,17 @@ set -x
 
 nc -zw 60 ${BOXROOT_FQDN} 22
 ./sysinstall
+# wait for box to go down first
+set +x
+while ping -c 1 ${BOXROOT_FQDN} &>/dev/null; do
+    echo -n 'W'
+done
+# wait until it gets back up again
+while ! ping -c 1 ${BOXROOT_FQDN} &>/dev/null; do
+    echo -n 'U'
+done
+set -x
+# starting sshd also takes a while, wait for it
 nc -zw 60 ${BOXROOT_FQDN} 22
 ./bootstrap
 #nc -zw 60 ${BOXROOT_FQDN} 22
